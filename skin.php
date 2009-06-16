2e8f6223a81b81505d702b696e9aaf2b99244500
David Gilbert <david@solidgone.com>
2009-06-16 02:24:06 +0000

    new: Added color themes. new: Works cross-browser. chg: Removed (:nosearchbar:) markup.

--- skin.php
+++ skin.php
@@ -10,9 +10,50 @@
 global $FmtPV, $SkinStyle, $PageLogoUrl, $PageLogoWidth, $RecipeInfo, $HTMLStylesFmt;
 $FmtPV['$SkinName'] = '"equilibrium"';
 $FmtPV['$SkinVersion'] = '"0.1.0"';
+$FmtPV['$SkinDate'] = '"20090615"';
 
+# Default color scheme
+global $SkinColor, $ValidSkinColors;
+$UserSkinColors = (is_array($ValidSkinColors) ?$ValidSkinColors :array());
+$ValidSkinColors['black'] = array('block-highlight-back'=>'#000','entry-title-text'=>'#0B96D0','text-highlight'=>'#0B96D0','block-highlight-text'=>'#fff','title-text'=>'#000');
+$ValidSkinColors['red'] = array('block-highlight-back'=>'#CC0000','entry-title-text'=>'#000','text-highlight'=>'#0B96D0','block-highlight-text'=>'#fff','title-text'=>'#000');
+$ValidSkinColors['orange'] = array('block-highlight-back'=>'#FF7400','entry-title-text'=>'#000','text-highlight'=>'#0B96D0','block-highlight-text'=>'#fff','title-text'=>'#000');
+$ValidSkinColors['dark-orange'] = array('block-highlight-back'=>'#D64B00','entry-title-text'=>'#000','text-highlight'=>'#0B96D0','block-highlight-text'=>'#fff','title-text'=>'#000');
+$ValidSkinColors['light-blue'] = array('block-highlight-back'=>'#4096EE','entry-title-text'=>'#000','text-highlight'=>'#0B96D0','block-highlight-text'=>'#fff','title-text'=>'#000');
+$ValidSkinColors['pink'] = array('block-highlight-back'=>'#FF0084','entry-title-text'=>'#000','text-highlight'=>'#0B96D0','block-highlight-text'=>'#fff','title-text'=>'#000');
+$ValidSkinColors['purple'] = array('block-highlight-back'=>'#80185B','entry-title-text'=>'#000','text-highlight'=>'#0B96D0','block-highlight-text'=>'#fff','title-text'=>'#000');
+$ValidSkinColors['gold'] = array('block-highlight-back'=>'#C79810','entry-title-text'=>'#000','text-highlight'=>'#0B96D0','block-highlight-text'=>'#fff','title-text'=>'#000');
+$ValidSkinColors['green'] = array('block-highlight-back'=>'#008C00','entry-title-text'=>'#000','text-highlight'=>'#0B96D0','block-highlight-text'=>'#fff','title-text'=>'#000');
+$ValidSkinColors['blue'] = array('block-highlight-back'=>'#356AA0','entry-title-text'=>'#000','text-highlight'=>'#0B96D0','block-highlight-text'=>'#fff','title-text'=>'#000');
+$ValidSkinColors['mint'] = array('block-highlight-back'=>'#CDEB8B','entry-title-text'=>'#000','text-highlight'=>'#0B96D0','block-highlight-text'=>'#fff','title-text'=>'#000');
+$ValidSkinColors['dark-blue'] = array('block-highlight-back'=>'#3F4C6B','entry-title-text'=>'#000','text-highlight'=>'#0B96D0','block-highlight-text'=>'#fff','title-text'=>'#000');
+$ValidSkinColors = array_merge($ValidSkinColors, $UserSkinColors);
+
+if ( isset($_GET['color']) && isset($ValidSkinColors[$_GET['color']]) ) {
+	$SkinColor = $_GET['color'];
+} elseif ( !isset($ValidSkinColors[$SkinColor]) ) {
+	$SkinColor = 'red';
+}
+
+$HTMLStylesFmt['equilibrium'] =
+	'.featured .title,.latest .title,.featured .title h2 a,.latest .title h2 a'.
+		'{color:'. $ValidSkinColors[$SkinColor]['entry-title-text']. '}'.
+	'.featured-title h2 a:hover,.featured .title h2 a:hover{color:'. $ValidSkinColors[$SkinColor]['block-highlight-text'].'}'.
+	'.featured .title,.latest .title{background:'. $ValidSkinColors[$SkinColor]['block-highlight-back'].'}'.
+	'p a,p a:visited,.postMeta-post a,.postMeta-post a:visited,.featured .post-content h2 a,'.
+	'.latest .post-content h2,#footer div a:hover,'.
+	'#comments .commentmetadata li a'.
+		'{color:'. $ValidSkinColors[$SkinColor]['text-highlight']. '}'.
+	'#sidebar a:hover,#nav a:hover,#top ul a:hover,ul#nav li.current_page_item a:link,'.
+	'ul#nav li.current_page_item a:visited,ul#nav li.current_page_item a:hover,ul#nav li.current_page_item a:active,'.
+	'#top ul li.current_page_item a:link,#top ul li.current_page_item a:visited,#top ul li.current_page_item a:hover,'.
+	'#top ul li.current_page_item a:active'.
+		'{color:'. $ValidSkinColors[$SkinColor]['block-highlight-text'].
+		';background:'. $ValidSkinColors[$SkinColor]['block-highlight-back'].'}'.
+	'h1.logo a, #top h1 a'.
+		'{color:'. $ValidSkinColors[$SkinColor]['title-text']. '}';
 if (!empty($PageLogoUrl) && !empty($PageLogoUrlWidth)) {
-	$HTMLStylesFmt['equilibrium'] = '#header h1 a {padding-left: ' .$PageLogoWidth .'; background: url(' .$PageLogoUrl .') left bottom no-repeat;}';
+	$HTMLStylesFmt['equilibrium'] .= '#header h1 a {padding-left: ' .$PageLogoWidth .'; background: url(' .$PageLogoUrl .') left bottom no-repeat;}';
 }
 global $WikiStyleApply;
 $WikiStyleApply['a'] = 'a';
@@ -20,8 +61,6 @@ $WikiStyleApply['a'] = 'a';
 # Move any (:noleft:) or SetTmplDisplay('PageLeftFmt', 0); directives to variables for access in jScript.
 $FmtPV['$RightColumn'] = "\$GLOBALS['TmplDisplay']['PageRightFmt']";
 Markup('noright', 'directives',  '/\\(:noright:\\)/ei', "SetTmplDisplay('PageRightFmt',0)");
-$FmtPV['$SearchBar'] = "\$GLOBALS['TmplDisplay']['PageSearchFmt']";
-Markup('nosearch', 'directives',  '/\\(:nosearch:\\)/ei', "SetTmplDisplay('PageSearchFmt',0)");
 $FmtPV['$ActionBar'] = "\$GLOBALS['TmplDisplay']['PageActionFmt']";
 Markup('noaction', 'directives',  '/\\(:noaction:\\)/ei', "SetTmplDisplay('PageActionFmt',0)");
 $FmtPV['$TabsBar'] = "\$GLOBALS['TmplDisplay']['PageTabsFmt']";
